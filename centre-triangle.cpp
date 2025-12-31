384879650b5e9badcc4216349a73ee5e3915ec25
hammad <hammadmustafa675@gmail.com>
2025-12-31 13:09:06 +0500

    added second pattern

--- centre-triangle.cpp
+++ centre-triangle.cpp
@@ -1,12 +1,12 @@
 /*
-Pattern: Right-Angled Triangle
+Pattern: centre Triangle
 Input: n = 5
 Output:
-*
-* *
-* * *
-* * * *
-* * * * * 
+    *
+   ***
+  *****
+ *******
+*********
 */
 
 
@@ -22,8 +22,11 @@ int main(){
 
     for(int i=1;i<=a;i++){
 
-        for(int j=1;j<=i;j++){
-            cout<<"* ";
+        for(int j=a-i;j>=1;j--){
+            cout<<" ";
+        }
+        for(int k=1;k<=2*i-1;k++){
+            cout<<"*";
         }
         cout<<endl;
     }
